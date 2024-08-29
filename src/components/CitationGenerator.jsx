874547dd02d2b0a111ad38e4919c521d988e1be4
katharina-spiecker <kathispiecker@gmail.com>
2024-08-29 13:24:27 +0200

    fix

--- src/components/CitationGenerator.jsx
+++ src/components/CitationGenerator.jsx
@@ -1,40 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Spinner from "./Spinner";
 
-
 export default function CitationGenerator() {
+    // Array mit allen Zitate
+    // wir wollen das React den Wert speichert aber
+    // die Benutzeroberfläche muss nicht aktualisiert werden bei Änderung
+    const quotesRef = useRef([]);
 
-    const [allQuotes, setAllQuotes] = useState([]);
+    // wir wollen das React den Wert speichert
+    // die Benutzeroberfläche muss aktualisiert werden bei Änderung
     const [activeQuote, setActiveQuote] = useState(null);
 
-    useEffect(function() {
+    // wird nur einmal ausgeführt
+    useEffect(() => {
         fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
         .then(response => response.json())
         .then(data => {
-            setAllQuotes(data.quotes);
-            setActiveQuote(data.quotes[0]);
+            quotesRef.current = data.quotes;
+            setActiveQuote(data.quotes[0])
         })
+
     }, [])
 
     function getRandomQuote() {
-        const randomIndex = Math.floor(Math.random() * allQuotes.length);
-        setActiveQuote(allQuotes[randomIndex]);
+        if (quotesRef.current.length > 0) {
+            const randomIndex = Math.floor(Math.random() * quotesRef.current.length);
+            setActiveQuote(quotesRef.current[randomIndex]);
+        }
     }
 
     return (
-        <div className="citation-box">
+        <div>
             {
                 activeQuote ? (
-                    <div>
-                        <h3>{activeQuote.quote}</h3>
-                        <p>- {activeQuote.author}</p>
-                    </div>
+                <div className="citation-box">
+                    <p>"{activeQuote.quote}"</p>
+                    <p>- {activeQuote.author}</p>
+                    <button className="citation-box__btn" onClick={getRandomQuote}>Next quote</button>
+                </div>
                 ) : (
-                    <Spinner />
-                )
-                
+                <Spinner />
+                ) 
             }
-            <button className="citation-box__btn" onClick={getRandomQuote}>Nächstes Zitat</button>
         </div>
     )
 }
